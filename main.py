209352faa6e5669fb09f10222afc71ec6865e402
Rafal.Py <rafalpilewicz@gmail.com>
2021-05-02 20:58:34 +0200

    Adding a second pandas dataframe

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import streamlit as st
 import pandas as pd
+import numpy as np
+import altair as alt
+import pydeck as pdk
 
 
 def print_hi(name):
@@ -19,9 +22,28 @@ def print_hi(name):
     desired_label = st.selectbox("Filter to:", ["car", "truck"])
     st.write(data[data.label == desired_label])
 
+# SETTING PAGE CONFIG TO WIDE MODE
+st.beta_set_page_config(layout="wide")
+
+# LOADING DATA
+DATE_TIME = "date/time"
+DATA_URL = (
+    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Streamlit')
+    #print_hi('Streamlit')
+    data = load_data(100000)
+    'data', data
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
