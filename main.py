00b4003f666c3a7144b6554465717c0b89b32bc1
Rafal.Py <rafalpilewicz@gmail.com>
2021-05-02 20:49:39 +0200

    Adding a first pandas dataframe

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ def print_hi(name):
     x = st.slider("First squared slider")
     st.write(x, 'squared is', x * x)
 
+    # Reuse this data across the runs!
+    read_and_cache_csv = st.cache(pd.read_csv)
+    BUCKET = f"https://streamlit-self-driving.s3-us-west-2.amazonaws.com/"
+    data = read_and_cache_csv(BUCKET + "labels.csv.gz", nrows=1000)
+    desired_label = st.selectbox("Filter to:", ["car", "truck"])
+    st.write(data[data.label == desired_label])
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
